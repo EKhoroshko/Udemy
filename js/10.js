14812353568a5a481bcd33936566f99a228c1ffa
EKhoroshko <xapoxa14@gmail.com>
2021-03-31 18:02:10 +0300

    operators

---
+++ js/10.js
@@ -0,0 +1,18 @@
+"use strict";
+
+// let x = 5; alert(x++); выдаст 5 и работает только в браузере
+// [] + false - null + true  ответ NaN
+//let y = 1; let x = y = 2; alert(x); последовательное присваивание ответ 2
+// [3] + 1 + 2; console.log([] + 1 + 2); конкатенация отв 12 стринг)
+//alert("1"[0]);
+//console.log(2 && 1 && null && 0 && undefined); и запинается на лжи
+// console.log(!!(1 && 2) === (1 && 2)); !! приведет к булю, и вернет нам False
+// alert(null || 2 && 3 || 4); (|| или) запинается на правде ответ:3
+/* const a = [1, 2, 3];
+const b = [1, 2, 3];
+console.log(a == b);  false*/
+//alert(+"Infinity");  унарный + скажет это число, а вывод будет Infinity
+// console.log("Ёжик" > "яблоко"); false
+
+// console.log(0 || "" || 2 || undefined || true || falsе); Ответ 2
+
